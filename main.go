7a4d51927919d4d32bb6601d8e46c89bc26b9733
Jeremy Price <jeremy.price@careem.com>
2019-01-16 08:13:52 +0100

    Attempting to add left column layout

--- main.go
+++ main.go
@@ -77,6 +77,48 @@ func StartNuke() (*glfw.Window, *nk.Context) {
 	return win, ctx
 }
 
+func LeftCol(win *glfw.Window, ctx *nk.Context, state interface{}, pane1, pane2, pane3 func()) {
+	//log.Println("Redraw")
+	maxVertexBuffer := 512 * 1024
+	maxElementBuffer := 128 * 1024
+
+	nk.NkPlatformNewFrame()
+
+	// Layout
+	bounds := nk.NkRect(50, 50, 230, 250)
+	update := nk.NkBegin(ctx, "GitRemind", bounds,
+		nk.WindowBorder|nk.WindowMovable|nk.WindowScalable|nk.WindowMinimizable|nk.WindowTitle)
+	nk.NkWindowSetPosition(ctx, "GitRemind", nk.NkVec2(0, 0))
+	nk.NkWindowSetSize(ctx, "GitRemind", nk.NkVec2(float32(winWidth), float32(winHeight)))
+
+	if update > 0 {
+		nk.NkLayoutRowBegin(ctx, nk.Dynamic, 400, 2)
+		log.Println("here")
+
+		nk.NkLayoutRowPush(ctx, 200)
+		nk.NkGroupBegin(ctx, "Group 1", nk.WindowBorder)
+		pane1()
+		nk.NkGroupEnd(ctx)
+
+		nk.NkLayoutRowPush(ctx, 400)
+		nk.NkGroupBegin(ctx, "Group 2", nk.WindowBorder)
+		pane2()
+		nk.NkLayoutRowEnd(ctx)
+
+	}
+	nk.NkEnd(ctx)
+
+	// Render
+	bg := make([]float32, 4)
+	//nk.NkColorFv(bg, state.bgColor)
+	width, height := win.GetSize()
+	gl.Viewport(0, 0, int32(width), int32(height))
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+	gl.ClearColor(bg[0], bg[1], bg[2], bg[3])
+	nk.NkPlatformRender(nk.AntiAliasingOn, maxVertexBuffer, maxElementBuffer)
+	win.SwapBuffers()
+}
+
 //3 pane layout - two small panes on the top, a large box below for displaying the contents of emails
 //
 //You provide the contents of panes 1, 2, and 3 as callback functions that take no args and return no values
@@ -131,6 +173,18 @@ func ButtonBox(ctx *nk.Context, pane1, pane2 func()) {
 		}
 		nk.NkGroupEnd(ctx)
 	}
+}
+
+//A horizontal button bar.  Fires the callback when clicked
+func ButtonBar(ctx *nk.Context, buttons []string, callback func(i int, s string)) {
+	//	nk.NkLayoutRowDynamic(ctx, 40, int32(len(buttons)))
+	//{
+	for i, v := range buttons {
+		if nk.NkButtonLabel(ctx, v) > 0 {
+			callback(i, v)
+		}
+	}
+	//}
 }
 
 //The Ratatosk layout
